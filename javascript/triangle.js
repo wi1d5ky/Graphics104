f58760e71d587354362ed60ca8885eece797a77f
wi1d5ky <poppin.wildsky@gmail.com>
2015-10-13 21:34:20 +0800

    Add gradient(HW1)

--- javascript/triangle.js
+++ javascript/triangle.js
@@ -6,62 +6,64 @@ function run()
     var canvas = document.getElementById( "gl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL 沒辦法用QQ。" ); }
-
-
-    // Three Vertices
-
-    var a,b,c;
-    a = Math.random()*2 - 1;
-    b = Math.random()*2 - 1;
-    c = Math.random()*2 - 1;
+    if ( !gl ) { alert( "WebGL 無法使用QQ。" ); }
 
     var vertices = [
-        vec2( a, 0 ),
-        vec2(  b, 1 ),
-        vec2(  c, -1 ),
+        vec3( -1, 0, 0 ), vec3(-0.65, 1, 0 ), vec3( -0.25, 0.25, 0 ),
+        vec3( -1, 0, 0 ), vec3(-0.65, -1, 0 ), vec3( -0.25, -0.25, 0 ),
 
-        vec2( 0, a ),
-        vec2(  1, b ),
-        vec2(  -1, c ),
+        vec3(-0.25, 0.25, 0), vec3(0, 1, 0 ), vec3( 0.25, 0.25, 0 ),
+        vec3(-0.25, -0.25, 0), vec3(0, -1, 0 ), vec3( 0.25, -0.25, 0 ),
 
-        vec2( a, a ),
-        vec2(  -b, b ),
-        vec2(  c, c )
+        vec3( 0.25, 0.25, 0 ), vec3( 0.65, 1, 0 ), vec3( 1, 0, 0 ),
+        vec3( 0.25, -0.25, 0 ), vec3( 0.65, -1, 0 ), vec3( 1, 0, 0 )
     ];
 
-    //
+    var colors = [
+        vec3( 1, 0, 0 ), vec3( 1, 0.5, 0 ), vec3( 1, 1, 0 ),
+        vec3( 1, 0, 0 ), vec3( 1, 0.5, 0 ), vec3( 1, 1, 0 ),
+
+        vec3( 0.75, 1, 0 ), vec3( 0, 1, 0 ), vec3( 0, 0.25, 0.75 ),
+        vec3( 0.75, 1, 0 ), vec3( 0, 1, 0 ), vec3( 0, 0.25, 0.75 ),
+
+        vec3( 0, 0, 1 ), vec3( 0.5, 0, 0.5 ), vec3( 1, 0, 1 ),
+        vec3( 0, 0, 1 ), vec3( 0.5, 0, 0.5 ), vec3( 1, 0, 1 )
+    ];
+
     //  Configure WebGL
-    //
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( Math.abs(a), Math.abs(b), Math.abs(c), 1.0 );
+    gl.clearColor( 0.75, 1.0, 1.0, 1.0 );
+    gl.clear( gl.COLOR_BUFFER_BIT );
 
     //  Load shaders and initialize attribute buffers
-
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
     // Load the data into the GPU
-
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 
     // Associate out shader variables with our data buffer
-
     var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
-    render();
+    // Load the data into the GPU
+    var cBufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+
+    for (var i = 0; i < 6; i++) {
+        gl.drawArrays( gl.TRIANGLES, i*3, 3 );
+    }
 };
 
-
-function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 9 );
-}
-
 window.onload = function() {
-    setInterval( run, 1000);
+    run();
 };
